b5b41d7a5fa4f39257f7463950934325f0afd9ff
glk <shashavrin@yandex.ru>
2026-07-24 21:51:15 +0500

    pc version fix

--- frontend/src/components/layouts/Container.jsx
+++ frontend/src/components/layouts/Container.jsx
@@ -1,6 +1,9 @@
-const Container = ({ children, className = "", maxWidth = "max-w-7xl" }) => {
+const Container = ({ children, className = "", maxWidth = "max-w-7xl", id }) => {
     return (
-        <div className={`w-full ${maxWidth} mx-auto px-5 sm:px-6 lg:px-8 ${className}`}>
+        <div
+            id={id}
+            className={`w-full ${maxWidth} mx-auto px-5 sm:px-6 lg:px-8 ${className}`}
+        >
             {children}
         </div>
     );
